3a73e2e1fb539eae875ec9eb2a2c825715c22bab
PalacioAF <federicopalacio92@gmail.com>
2021-03-04 12:05:19 -0300

    pop up - back

--- js/form.js
+++ js/form.js
@@ -27,6 +27,43 @@ function validate(form) {
         return false;
     }
     else {
-        return confirm('Enviar consulta?');
+        popup();
+        return false;
     }
 };
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+ function popup() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+    hrefIndex();
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      hrefIndex();
+    }
+  }
+
+  function hrefIndex(){
+    const url= window.location.href.replace("form.html", "index.html")
+    window.location.href = url
+  }
+
+
+
+  if(history.forward(1))
+  location.replace(history.forward(1));
